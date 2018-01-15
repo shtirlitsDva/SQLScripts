629e2763183c967105907661c1339745270bddf8
shtirlitsDva <xopkop@gmail.com>
2018-01-15 10:23:26 +0100

    Update Plant3D_Prepend.sql

--- Plant3D_Prepend.sql
+++ Plant3D_Prepend.sql
@@ -2,3 +2,10 @@
 update EngineeringItems --Be careful on live database!!!
 set Mark = '91' + Mark
 where not Mark = ''
+
+--Find all tables with column 'Tag' to be able to modify tag value directly
+SELECT      c.name  AS 'ColumnName', t.name AS 'TableName'
+FROM        sys.columns c
+JOIN        sys.tables  t   ON c.object_id = t.object_id
+WHERE       c.name = 'Tag' AND NOT (t.name = 'PnPTagRegistry' OR t.name = 'PipeLines' OR t.name = 'PipeLineGroup')
+ORDER BY    TableName, ColumnName;
